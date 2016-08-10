74a934e1624fd47d2b7df79b44338065b2f2251d
zerox22x <collinbakker@gmail.com>
2016-08-10 12:05:51 +0200

    current state , having trouble with compare-object

--- Diagnostic/loose scripts/copyuserfolder.ps1
+++ Diagnostic/loose scripts/copyuserfolder.ps1
@@ -1,21 +1,23 @@
 #paths
-$source = "E:\versionrecall\VersionRecall Repositories"
-
-$destination = "E:\versionrecall\test"
+$source = Read-host "Type path to source"
+$destination = read-host "path to destination"
+#$source = Get-ChildItem -Path (Read-Host -Prompt 'type path to file or folder') |Out-string
+#$destination = Get-ChildItem -Path (Read-Host -Prompt 'type destination , if it was a file enter filename aswell') | Out-string
 New-Item $destination -type directory
-
-$files = gci -path $source -recurse
-$A = $files|Get-filehash 
+set-location $source
+$files = gci  -recurse
+$A = $files|Get-filehash | Select-object -property hash
 Start-Sleep -Milliseconds 300
 #Copy-Item $source -Destination $destination -Recurse
 #Import-Module BitsTransfer
 #Start-BitsTransfer -Source $source -Destination $destination -Description "Backup" -DisplayName "Backup"
-#robocopy  $source  $destination /X
-xcopy $source $destination /E /-Y /L /v /Z
+# robocopy from $source to destination , show eta , prompt for overwrite, /E copy everything including empty folders , 
+robocopy $source $destination /E /ETA /R:0 /W:0
+#xcopy $source $destination /E /-Y /L /v /Z
 Start-Sleep -Milliseconds 300
-$target = $destination
-$files2 = gci -path $target -recurse
-$B = $files2|Get-filehash 
+set-location $destination
+$files2 = gci -recurse
+$B = $files2|Get-filehash |Select-object -property hash
 
 $result = Compare-Object $A $B |Select-Object -property *
 if ($result.Inputobject -like "*") {$results ; Write-host no output means all matches}
